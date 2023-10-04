730871be85db24a1c542a27ad2f20af3a549830c
Rakha <rakhaahmed1@gmail.com>
2023-10-05 01:42:35 +0300

    add new

--- java.js
+++ java.js
@@ -81,7 +81,6 @@ cardito1.addEventListener('click',function(){
     title.textContent=cards[0].title;
     descripion.textContent=cards[0].descripion;
     img.src=cards[0].img;
-    // afterPseudo.backgroundColor='red';
 
 })
 cardito2.addEventListener('click',function(){
@@ -108,4 +107,62 @@ cardito5.addEventListener('click',function(){
     img.src=cards[4].img;
 
 })
-// for (let counter=0;counter<cards.length;counter++)
+const cards2=[
+    {
+        id:"card1",
+        title: "Para ti, Vodafone Flex",
+        text:"Consigue tus móviles y dispositivos en Vodafone a un precio especial. Podrás pagar a plazos sin intereses, ahorrar beneficiándote de re-estrena, contratar tu seguro móvil o el servicio multisim de one number y todo con la tranquilidad de nuestro servicio postventa.",
+        hyperlink: "Más info",
+        img:"Images/seguro movil.jpeg"
+    },
+    {
+        id:"card2",
+        title: "Datos sin límites con velocidad 5G",
+        text:"No hace falta que busques WiFi ni mires el calendario, con tu móvil podrás conectarte desde el primer hasta el último día del mes a la misma velocidad en red 5G con gigas ilimitados. Consume vídeo, música y cualquier contenido sin preocuparte si te quedarás sin datos.",
+        hyperlink: "Ver Tarifas",
+        img:"Images/tecnologia-5g-ilimitada.jpeg"
+
+    },
+    {
+        id:"card3",
+        title: "Toda la TV está en Vodafone",
+        text:"Con Vodafone TV disfrutarás del mejor cine y series donde y como quieras, o bien con nuestro deco 4K o en todos tus dispositivos con la app de Vodafone TV online.Las mejores películas y series de HBO Max, Disney+, Amazon Prime Video, Filmin, Netflix y muchas más disponibles en el menú.",
+        hyperlink:"Más info",
+        img:"Images/el-mejor-cine.jpeg"
+    }
+]
+container5Tiltle= document.querySelector(".container5-title");
+container5Text= document.querySelector(".container5-text");
+container5Span= document.querySelector(".container5-span");
+container5Image= document.querySelector(".container5-image");
+x=document.querySelectorAll(".container5-ul li");
+y=document.querySelectorAll(".lo-mas-buscado button")
+
+for (let k=0; k < x.length; k++){
+    x[k].addEventListener("click",function(){
+        container5Tiltle.textContent=cards2[k].title;
+        container5Text.textContent=cards2[k].text;
+        container5Span.textContent=cards2[k].hyperlink;
+        container5Image.src=cards2[k].img;
+    })
+}
+x.forEach(function(ele){
+    ele.onclick=function(){
+        x.forEach(function(ele){
+            ele.classList.remove('li-underline')
+        })
+        this.classList.add("li-underline")
+    };
+});
+
+
+y.forEach(function(ele){
+    ele.onclick=function(){
+        y.forEach(function(ele){
+            ele.classList.remove("lo-mas-buscado-js")
+        })
+    ele.classList.add("lo-mas-buscado-js")
+        // this.classList.add("li-underline")
+    };
+});
+
